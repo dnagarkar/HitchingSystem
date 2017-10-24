37d7f4300de06000932709822ea5c6424da4b656
dnagarkar <devashri@gmail.com>
2017-10-24 15:49:00 -0500

    fixed errors

--- arrowHandling.py
+++ arrowHandling.py
@@ -60,16 +60,23 @@ import sys, pygame
 
 def moveright(baler_rect):
   #move baler left
-  baler_rect= baler_rect.move([-2, 0])
+  baler_rect= baler_rect.move([-15, 0])
   # screen.blit(baler, baler_rect)
   return baler_rect
   #rotate lines to right
 
 def moveleft(baler_rect):
   #move baler right
-  baler_rect= baler_rect.move([2, 0])
+  baler_rect= baler_rect.move([15, 0])
   return baler_rect
   # screen.blit(baler, baler_rect)
+
+def moveup(tractor_rect, track_rect):
+  #move baler right
+  tractor_rect= tractor_rect.move([0, -5])
+  track_rect= track_rect.move([0, -5])
+
+  return tractor_rect, track_rect
 
 def rot_center(image, angle):
     """rotate an image while keeping its center and size"""
@@ -119,8 +126,14 @@ while 1:
       print('right')
       baler_rect= moveright(baler_rect)
       trackAngle = trackAngle - 10
-  baler_rect= baler_rect.move(baler_speed)
-  #track_rect= track_rect.move(speed)
+    if event.key == pygame.K_UP:
+      print('up')
+      tractor_rect, track_rect= moveup(tractor_rect, track_rect)
+
+  #baler_rect= baler_rect.move(baler_speed)
+  track_rect= track_rect.move(speed)
+  tractor_rect= tractor_rect.move(speed)
+
   track1= rot_center(track, trackAngle) #rotates 
   screen.fill(white)
   screen.blit(tractor, tractor_rect)
