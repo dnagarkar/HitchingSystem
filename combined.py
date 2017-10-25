e73c1c41ee85c0635b0e0be5a0cc586b06ee3d46
dnagarkar <devashri@gmail.com>
2017-10-24 20:44:43 -0500

    combining animation with sensors

--- combined.py
+++ combined.py
@@ -17,6 +17,36 @@ GPIO.setup(ECHO_LEFT, GPIO.IN)
 GPIO.setup(TRIG_RIGHT, GPIO.OUT)
 GPIO.setup(ECHO_RIGHT, GPIO.IN)
 
+
+def moveright(baler_rect):
+  #move baler left
+  baler_rect= baler_rect.move([-15, 0])
+  # screen.blit(baler, baler_rect)
+  return baler_rect
+  #rotate lines to right
+
+def moveleft(baler_rect):
+  #move baler right
+  baler_rect= baler_rect.move([15, 0])
+  return baler_rect
+  # screen.blit(baler, baler_rect)
+
+def moveup(tractor_rect, track_rect):
+  #move baler right
+  tractor_rect= tractor_rect.move([0, -5])
+  track_rect= track_rect.move([0, -5])
+
+  return tractor_rect, track_rect
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
 def distance(GPIO_TRIGGER, GPIO_ECHO):
     # set Trigger to HIGH
     GPIO.output(GPIO_TRIGGER, True)
@@ -103,7 +133,7 @@ trackAngle = 0
 i= 0
 
 while(1):
-    DIST_LEFT = distance(TRIG_LEFT, ECHO_LEFT)
+	DIST_LEFT = distance(TRIG_LEFT, ECHO_LEFT)
     DIST_RIGHT = distance(TRIG_RIGHT, ECHO_RIGHT)
 
     if abs(DIST_LEFT - DIST_RIGHT) <= error:
@@ -112,3 +142,31 @@ while(1):
         print "turn left"
     else:
         print "turn right"
+   	for event in pygame.event.get():
+    	if event.type == pygame.QUIT: sys.exit()
+
+	#tractor_rect= tractor_rect.move(speed)
+  	if event.type == pygame.KEYDOWN:
+    	if event.key == pygame.K_LEFT:
+      		print('left')
+      		baler_rect= moveleft(baler_rect)
+      		trackAngle = trackAngle + 10
+    	elif event.key == pygame.K_RIGHT:
+      		print('right')
+      		baler_rect= moveright(baler_rect)
+      		trackAngle = trackAngle - 10
+    	if event.key == pygame.K_UP:
+      		print('up')
+      		tractor_rect, track_rect= moveup(tractor_rect, track_rect)
+
+  	#baler_rect= baler_rect.move(baler_speed)
+  	track_rect= track_rect.move(speed)
+  	tractor_rect= tractor_rect.move(speed)
+
+  	track1= rot_center(track, trackAngle) #rotates 
+  	screen.fill(white)
+  	screen.blit(tractor, tractor_rect)
+  	screen.blit(baler, baler_rect)
+  	screen.blit(track1, track_rect)
+  	pygame.display.flip()
+  	i=i+1
